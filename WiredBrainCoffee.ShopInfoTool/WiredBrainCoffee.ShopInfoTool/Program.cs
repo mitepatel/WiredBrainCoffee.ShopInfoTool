098a52dd3f8160e9ab77a5ca11919b4fe56289ac
Mitesh Patel <mitesh.patel@global.ntt>
2020-06-18 22:25:00 -0400

    Program file updated

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -7,6 +7,7 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+            Console.WriteLine("Added Data Access");
         }
     }
 }
